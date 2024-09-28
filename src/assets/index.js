6395af7062a1e54961e30ec3cbb0863af9818d49
eNKay <nguyenkhai24082408@gmail.com>
2024-09-29 02:04:30 +0700

    chore: Update assets folder
    - Add images and icons for social media, roles, technologies, projects and achievements
    - Add other important assets like logo, background, menu icon,...

---
+++ src/assets/index.js
@@ -0,0 +1,72 @@
+import logo from "./logo.png";
+import live from "./live.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import linkedin from "./socials/linkedin.png";
+import github from "./socials/github.png";
+import facebook from "./socials/facebook.png";
+
+import fullstack from "./roles/fullstack.png";
+import software from "./roles/software.png";
+import devops from "./roles/devops.png";
+import lead from "./roles/lead.png";
+
+import docker from "./tech/docker.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.png";
+import aws from "./tech/aws.png";
+import express from "./tech/express.png";
+import gitlab from "./tech/gitlab.png";
+import java from "./tech/java.png";
+import jenkins from "./tech/jenkins.png";
+import mysql from "./tech/mysql.png";
+import spring from "./tech/spring.png";
+
+import certificates from "./achievements/certificates.png";
+import education from "./achievements/education.png";
+import awards from "./achievements/awards.png";
+import experience from "./achievements/experience.png";
+import link from "./achievements/link.png";
+
+import digitalbank from "./projects/digital-bank.png";
+import propertytracker from "./projects/property-tracker.png";
+import ecommerce from "./projects/e-commerce.png";
+
+export {
+	logo,
+	fullstack,
+	software,
+	devops,
+	lead,
+	github,
+	menu,
+	close,
+	docker,
+	javascript,
+	mongodb,
+	reactjs,
+	tailwind,
+	threejs,
+	certificates,
+	education,
+	awards,
+	experience,
+	link,
+	aws,
+	express,
+	gitlab,
+	java,
+	jenkins,
+	mysql,
+	spring,
+	digitalbank,
+	propertytracker,
+	ecommerce,
+	live,
+	linkedin,
+	facebook,
+};
