a737fae42a533cbb371de5447d5d04d07b55b564
eNKay <nguyenkhai24082408@gmail.com>
2024-11-10 00:34:42 +0700

    chore: Update Hero component text
    - Change 'Full-Stack Developer' to 'Software Engineer' in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
 						Hi, I'm <span className="text-[#2f80ed]">eNKay</span>
 					</h1>
 					<p className={`${styles.heroSubText} mt-2`}>
-						A Full-Stack Developer passionate about{" "}
+						A Software Engineer passionate about{" "}
 						<br className="sm:block hidden" />
 						seamless web solutions and DevOps practices
 					</p>
