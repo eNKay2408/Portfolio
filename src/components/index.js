1857e98c6a9231e5adc8d2855b7eba812bad5a3a
eNKay <nguyenkhai24082408@gmail.com>
2024-11-10 00:33:46 +0700

    feat: Add main component exports
    - Export main components including Hero, Navbar, About, Tech, Achievements, Projects, Feedbacks, Contact, Footer, and canvas components

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import {
+	EarthCanvas,
+	BallCanvas,
+	ComputersCanvas,
+	StarsCanvas,
+} from "./canvas";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Achievements from "./Achievements";
+import Projects from "./Projects";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+export {
+	Hero,
+	Navbar,
+	About,
+	Tech,
+	Achievements,
+	Projects,
+	Feedbacks,
+	Contact,
+	Footer,
+	EarthCanvas,
+	BallCanvas,
+	ComputersCanvas,
+	StarsCanvas,
+};
